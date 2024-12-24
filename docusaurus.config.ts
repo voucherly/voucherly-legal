354abfa9e70856eef80066c7899502514e1d6778
Francesco Bognini <info@francescobognini.com>
2024-12-24 15:50:25 +0100

    Trailing slash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,19 +8,15 @@ const config: Config = {
   tagline: 'Il modo migliore per accettare i buoni pasto',
   favicon: 'https://ucarecdn.com/5c304dd8-db4f-471e-a930-04bc0ed961d3/-/preview/100x100/',
 
-  // Set the production url of your site here
   url: 'https://legal.voucherly.it',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'voucherly', // Usually your GitHub org/user name.
-  projectName: 'voucherly-legal', // Usually your repo name.
+  organizationName: 'voucherly',
+  projectName: 'voucherly-legal',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -47,6 +43,11 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
     image: 'https://ucarecdn.com/1a50448f-55d8-46ac-bab0-764dee2b9c4f/-/preview/100x100/',
     navbar: {
       logo: {
